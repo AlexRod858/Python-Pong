bf0e2551ea55128b49ea09fb71e8b33f949353e8
AlexRod <oa.defrag@gmail.com>
2023-11-17 21:41:28 +0000

    clase campo con todas sus lineas realizada

--- Campo.py
+++ Campo.py
@@ -1,10 +1,22 @@
 import pygame 
 
-def campo(screen):
+class Campo:
+    def __init__(self, screen):
+        self.screen = screen
 
-    screen.fill('green')  # ACTUALIZA FONDO
-    pygame.draw.line(screen, (255, 255, 255), (screen.get_width() // 2, 0), (screen.get_width() // 2, screen.get_height()), 2)
-
-
-    # pygame.draw.ellipse(screen, (0, 0, 255), (screen.get_width() // 2, screen.get_height() // 2), (200, 200), width=10)
-    pygame.draw.ellipse(screen, (0, 0, 255), (screen.get_width() // 2, screen.get_height() // 2, 200, 200), width=10)
+    def dibujar_campo(self):
+        self.screen.fill('green')
+        # BORDE
+        pygame.draw.rect(self.screen,'white', (10, 10,self.screen.get_width()-20,self.screen.get_height() -20), width= 4)
+        # MITAD
+        pygame.draw.line(self.screen, 'white', (self.screen.get_width() // 2, 10), (self.screen.get_width() // 2, self.screen.get_height()-14), 4)
+        # CENTRO
+        pygame.draw.circle(self.screen, 'white', (self.screen.get_width() // 2, self.screen.get_height() // 2), 50, width=4)
+        # AREA IZQ GRANDE
+        pygame.draw.rect(self.screen,'white', (10,self.screen.get_height() // 4,120,self.screen.get_height() // 2), width= 4)
+        # AREA DER GRANDE
+        pygame.draw.rect(self.screen,'white', (self.screen.get_width()- 130, self.screen.get_height() // 4,120,self.screen.get_height() // 2), width= 4)
+        # AREA IZQ PEQ
+        pygame.draw.rect(self.screen,'white', (10,self.screen.get_height() // 3,40,self.screen.get_height() // 3), width= 4)
+        # AREA DER PEQ
+        pygame.draw.rect(self.screen,'white', (self.screen.get_width()- 50, self.screen.get_height() // 3,40,self.screen.get_height() // 3), width= 4)
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 import sys
 from Clases.bola import Bola
 from Clases.pad import Pad
-from Campo import campo
+from Campo import Campo
 # ------------------------------
 # ------------------------------
 # ------------------------------
@@ -10,17 +10,17 @@ from Campo import campo
 pygame.init()
 # ------------------------------
 # Configuración de la pantalla
-screen_width, screen_height = 800, 600
+screen_width, screen_height = 960, 480
 screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("Bola en Pygame")
+pygame.display.set_caption("Pong Game por AlexRod")
 
 # ------------------------------
 # ------------------------------
 # ----C R E O  O B J E T O S----
 # ------------------------------
 bola = Bola(400, 300, 40, "black", vel_x = 0.2, vel_y= 0.2)
-pad1 = Pad(50, 50, 20, 120)
-pad2 = Pad(730, 50, 20, 120)
+pad1 = Pad(20, 50, 20, 120)
+pad2 = Pad(920, 50, 20, 120)
 
 # ------------------------------
 # ------------------------------
@@ -38,7 +38,7 @@ while running:
     # Actualizar la posición y manejar la puntuación de la bola
     bola.movimiento(screen)
     # screen.fill('green')  # ACTUALIZA FONDO
-    campo(screen)
+    Campo(screen).dibujar_campo()
     # Resto del código para dibujar y gestionar la lógica del juego
     bola.dibujar(screen)
     pad1.dibujar(screen)
@@ -53,8 +53,8 @@ while running:
 
 
     # campo.campo()
-    puntuacion_texto = font.render(f'{puntuacion_pad1} - {puntuacion_pad2}', True, (255, 255, 255))
-    screen.blit(puntuacion_texto, (screen.get_width() // 2 - puntuacion_texto.get_width() // 2, 10))
+    puntuacion_texto = font.render(f'{puntuacion_pad1} - {puntuacion_pad2}', True, 'purple')
+    screen.blit(puntuacion_texto, (screen.get_width() // 2 - puntuacion_texto.get_width() // 2, 16))
 
     # Actualizar la pantalla
     pygame.display.flip()
